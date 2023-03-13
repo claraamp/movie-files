020b8a82388a114e95e4e1b95f4a8d1be6ab6bd8
Clara Andrade <mca9911@outlook.com>
2023-03-13 20:55:23 -0300

    fix: included key for api

--- src/movie-files/moviedatabase/movies.service.ts
+++ src/movie-files/moviedatabase/movies.service.ts
@@ -8,7 +8,7 @@ export class MoviesService {
     constructor(private readonly httpService: HttpService) {}
 
     async GetMovies(data: MoviesResponse) {
-        const response = await lastValueFrom(this.httpService.get('https://api.themoviedb.org/3/movie/top_rated'))
+        const response = await lastValueFrom(this.httpService.get('https://api.themoviedb.org/3/movie/top_rated?api_key=b9da6f1a1fbda60569b9015dc3d080bc'))
         console.log(response.data);
         return response.status == 200;
     }
